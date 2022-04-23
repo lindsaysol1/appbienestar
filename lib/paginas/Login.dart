a784d847a0ca18ef97f22232637725a8487aa0b0
Lindsay <lindsaysolanopalacio@gmail.com>
2022-04-23 13:29:08 -0500

    Actulización del LogIn

--- lib/paginas/Login.dart
+++ lib/paginas/Login.dart
@@ -8,13 +8,55 @@ class Login extends StatelessWidget {
   Widget build(context) {
 
     return Scaffold(
-        body: Container(
+        body:
+        Container(
             child: Column(
               children: [
                 Container(
+                  height: MediaQuery.of(context).size.height*0.3,
+                  width: MediaQuery.of(context).size.width,
                   child: Image.asset('assets/images/bienestar.png'),
-                ),
+                 ),
                 
+                Container(
+                  child: Text('Bienvendios',
+                    style: TextStyle(color: Colors.blueGrey[900], fontSize: 30,),
+                  ),
+                ),
+
+            Container(
+              margin: EdgeInsets.only(left: 40, right: 40, top: 0,bottom: 10),
+                child: Column(
+                  children: [
+                    Padding(padding: EdgeInsets.only(top:30)),
+                    TextFormField(
+                     style: TextStyle(color: Colors.black, fontSize: 16,),
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(7.0)),
+                          prefix: Container(
+                            width: 10,
+                            height: 23,
+                          ),
+                          hintText:'Nombre/correo Institucional',
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top:20)),
+                    TextFormField(
+                      obscureText:true,
+                      style: TextStyle( fontSize: 16,),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(7.0)),
+                        prefix: Container(
+                          width: 10,
+                          height: 23,
+                        ),
+                        hintText:'Contraseña',
+                      ),
+                    ),
+                  ],
+
+                ),
+            ),
               ],
             ),
             ),
